6d3e65e184452ba3637d55e052bf4318557c3f3c
herbetjr <herbetjr@gmail.com>
2020-06-03 15:16:26 -0300

    dsd

--- resources/views/site/layouts/template.blade.php
+++ resources/views/site/layouts/template.blade.php
@@ -13,8 +13,8 @@
   <link href="https://fonts.googleapis.com/css?family=Nunito:400,700&display=swap" rel="stylesheet">
 
   <!-- Template CSS -->
-  <link rel="stylesheet" href="css/style-starter.css">
-  <link rel="stylesheet" href="css/style-novo.css">
+  <link rel="stylesheet" href="/css/style-starter.css">
+  <link rel="stylesheet" href="/css/style-novo.css">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
