b53614672a33c3c66c241c5406899e8522595844
herbetjr <herbetjr@gmail.com>
2020-11-02 16:25:18 -0300

    DSADS

--- resources/views/site/page/projetos.blade.php
+++ resources/views/site/page/projetos.blade.php
@@ -17,7 +17,55 @@
                                 <div class="icon-holder">
                                     <span class="fa fa-folder service-icon" aria-hidden="true"></span>
                                 </div>
-                                <h4 class="mission">FRM - ADVOCACIA</h4>
+                                <h4 class="mission">FMR - ADVOCACIA</h4>
+                            </div>
+                        </div>
+                        <a>
+                </div>
+                <div class="col-lg-3 col-sm-6 bottom-gds">
+                    <a href="https://drive.google.com/drive/folders/1iY-1NMERMnSGWl3WIyiyfSOtzIr5WOAb?usp=sharing" target="_blank">
+                        <div class="elite-services1">
+                            <div class="bott-img">
+                                <div class="icon-holder">
+                                    <span class="fa fa-folder service-icon" aria-hidden="true"></span>
+                                </div>
+                                <h4 class="mission">TEMPLO DE DELFOS</h4>
+                            </div>
+                        </div>
+                        <a>
+                </div>
+                <div class="col-lg-3 col-sm-6 bottom-gds">
+                    <a href="https://drive.google.com/drive/folders/1vYPfvXlyP1UTn-k4kgZMIslj1xQmlwEP?usp=sharing" target="_blank">
+                        <div class="elite-services1">
+                            <div class="bott-img">
+                                <div class="icon-holder">
+                                    <span class="fa fa-folder service-icon" aria-hidden="true"></span>
+                                </div>
+                                <h4 class="mission">ESSÊNCIAS COSMÉTICOS</h4>
+                            </div>
+                        </div>
+                        <a>
+                </div>
+                <div class="col-lg-3 col-sm-6 bottom-gds">
+                    <a href="https://drive.google.com/drive/folders/1t6QNW8aj1XWvraxXs4f_bIl5ioT7t1xR?usp=sharing" target="_blank">
+                        <div class="elite-services1">
+                            <div class="bott-img">
+                                <div class="icon-holder">
+                                    <span class="fa fa-folder service-icon" aria-hidden="true"></span>
+                                </div>
+                                <h4 class="mission">LIMOEIRO FEST</h4>
+                            </div>
+                        </div>
+                        <a>
+                </div>
+                <div class="col-lg-3 col-sm-6 bottom-gds">
+                    <a href="https://drive.google.com/drive/folders/1xae-VULbwuegQ3j80ioWfFfrZNVq3Egn?usp=sharing" target="_blank">
+                        <div class="elite-services1">
+                            <div class="bott-img">
+                                <div class="icon-holder">
+                                    <span class="fa fa-folder service-icon" aria-hidden="true"></span>
+                                </div>
+                                <h4 class="mission">MO DOCINHO</h4>
                             </div>
                         </div>
                         <a>
