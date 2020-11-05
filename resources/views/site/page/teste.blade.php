d3bc129fee37a87636dcdf812139e41d4b408003
philldeveloper <philldeveloper@gmail.com>
2020-11-05 18:14:42 -0300

    teste

---
+++ resources/views/site/page/teste.blade.php
@@ -0,0 +1,101 @@
+<!doctype html>
+<html lang="pt_BR">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>Atena Agencia - Full Service</title>
+
+    <!-- google fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:400,700&display=swap" rel="stylesheet">
+
+    <!-- Template CSS -->
+    <link rel="stylesheet" href="css/style-starter.css">
+    <link rel="stylesheet" href="css/style-novo.css">
+
+<style>
+.img-circle {
+    border-radius: 50%;
+}
+.img-circle {
+    border-radius: 0;
+}
+
+.ratio {
+    background-position: center center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 0;
+    padding-bottom: 100%;
+    position: relative;
+    width: 100%;
+}
+.img-circle {
+    border-radius: 50%;
+}
+.img-responsive {
+    display: block;
+    height: auto;
+    max-width: 100%;
+}
+.table-bordered th, .table-bordered td {
+    border: 1px solid purple;
+}
+</style>
+</head>
+
+<body class="bg-purple p-5">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <div class="card">
+                    <div class="card-body p-5">
+                        <div class="form-group row justify-content-between">
+                            <h1 class="font-weight-bold">Atena Run Ranking</h1>
+                            <p class="lead">Atualizado em: 05/11/2020</p>
+                        </div>
+                        <div class="table-responsive">
+                            <table class="table table-hover table-bordered">
+                                <thead class="bg-purple text-light border-0">
+                                    <tr>
+                                        <th scope="col">Posição</th>
+                                        <th scope="col">Perfil</th>
+                                        <th scope="col">Corredor</th>
+                                        <th scope="col">Pista</th>
+                                        <th scope="col">Atena Coins</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <th scope="row" class="font-weight-bold h4">1</th>
+                                        <td><a href="#" class="ratio img-responsive img-circle" style="background: url('https://images.pexels.com/photos/450035/pexels-photo-450035.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260');"></a></td>
+                                        <td class="font-weight-bold h4">Jamerson</td>
+                                        <td class="font-weight-bold h4">Atena Store</td>
+                                        <td class="font-weight-bold text-purple h4">0</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row" class="font-weight-bold h4">2</th>
+                                        <td><a href="#" class="ratio img-responsive img-circle" style="background: url('https://images.pexels.com/photos/450035/pexels-photo-450035.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260');"></a></td>
+                                        <td class="font-weight-bold h4">Max</td>
+                                        <td class="font-weight-bold h4">Atena Store</td>
+                                        <td class="font-weight-bold text-purple h4">0</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row" class="font-weight-bold h4">3</th>
+                                        <td><a href="#" class="ratio img-responsive img-circle" style="background: url('https://images.pexels.com/photos/450035/pexels-photo-450035.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260');"></a></td>
+                                        <td class="font-weight-bold h4">Moisés</td>
+                                        <td class="font-weight-bold h4">Atena Store</td>
+                                        <td class="font-weight-bold text-purple h4">0</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/teste', function () {
+    return view('site.page.teste');
+});
+
 Route::get('/', function () {
     return view('site.page.index');
 });
